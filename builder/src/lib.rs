625bf35d180512348fffcb4fd35d48e8da8c3815
Reza Kakhki <reza.kakhki@grover.com>
2025-08-02 18:44:38 +0200

    pass builder macro test 7

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,15 +1,18 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Field, Type};
+use syn::{parse2, parse_macro_input, spanned::Spanned, DeriveInput, Field, Type};
 
-fn extract_option_inner_type(field: &Field) -> Option<&Type> {
+fn extract_inner_type<'a>(field: &'a Field, ident_type: &str) -> Option<&'a Type> {
+    if ident_type == "Vec" {
+        eprintln!("field: {:#?}", field);
+    }
     let path = match &field.ty {
         Type::Path(type_path) => &type_path.path,
         _ => return None,
     };
 
     let segment = path.segments.first()?;
-    if segment.ident != "Option" || path.segments.len() != 1 {
+    if segment.ident != ident_type || path.segments.len() != 1 {
         return None;
     }
 
@@ -25,7 +28,7 @@ fn extract_option_inner_type(field: &Field) -> Option<&Type> {
 }
 
 fn is_option_type(field: &Field) -> bool {
-    extract_option_inner_type(field).is_some()
+    extract_inner_type(field, "Option").is_some()
 }
 
 fn generate_builder_field(field: &Field) -> proc_macro2::TokenStream {
@@ -39,10 +42,44 @@ fn generate_builder_field(field: &Field) -> proc_macro2::TokenStream {
     }
 }
 
+fn get_each_attribute_clean(field: &Field) -> Option<String> {
+    for attr in &field.attrs {
+        let list = match &attr.meta {
+            syn::Meta::List(list) if list.path.is_ident("builder") => list,
+            _ => continue,
+        };
+        let parsed: syn::Result<EachAttr> = parse2(list.tokens.clone());
+        if let Ok(each_attr) = parsed {
+            return Some(each_attr.value);
+        }
+    }
+    None
+}
+
+struct EachAttr {
+    value: String,
+}
+impl syn::parse::Parse for EachAttr {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ident: syn::Ident = input.parse()?;
+        if ident != "each" {
+            return Err(syn::Error::new_spanned(ident, "expected `each`"));
+        }
+
+        let _eq: syn::Token![=] = input.parse()?;
+        let value: syn::LitStr = input.parse()?;
+
+        Ok(EachAttr {
+            value: value.value(),
+        })
+    }
+}
+
 fn generate_setter_method(field: &Field) -> proc_macro2::TokenStream {
     let name = &field.ident;
+    // pub fn arg(value: string) -> Self
 
-    if let Some(inner_ty) = extract_option_inner_type(field) {
+    if let Some(inner_ty) = extract_inner_type(&field, "Option") {
         // For Option<T> fields, setter takes T
         quote! {
             pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
@@ -53,10 +90,37 @@ fn generate_setter_method(field: &Field) -> proc_macro2::TokenStream {
     } else {
         // For T fields, setter takes T
         let ty = &field.ty;
-        quote! {
-            pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = ::std::option::Option::Some(#name);
-                self
+        match get_each_attribute_clean(&field) {
+            Some(each_attr) => {
+                let each_attr = syn::Ident::new(&each_attr, field.span());
+                let inner_type =
+                    extract_inner_type(&field, "Vec").expect("Can't extract vectro type");
+                // println!(
+                //     "HERE IS THE INNER VECTOR TYPE: {:?} {:?} {:?}",
+                //     inner_type, each_attr, name
+                // );
+                quote! {
+                    pub fn #each_attr(&mut self, #each_attr: #inner_type) -> &mut Self {
+                        match self.#name {
+                            Some(ref mut v) => {
+                                v.push(#each_attr);
+                                self
+                            }
+                            _ => {
+                                self.#name = Some(vec![#each_attr]);
+                                self
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {
+                quote! {
+                    pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                        self.#name = ::std::option::Option::Some(#name);
+                        self
+                    }
+                }
             }
         }
     }
@@ -72,11 +136,20 @@ fn generate_build_field(field: &Field) -> proc_macro2::TokenStream {
         }
     } else {
         // Required fields: unwrap with error message
-        quote! {
-            #name: self.#name.clone()
-                .ok_or_else(|| ::std::boxed::Box::<dyn ::std::error::Error>::from(
-                    ::std::format!("field `{}` is not set", ::std::stringify!(#name))
-                ))?
+        match get_each_attribute_clean(&field) {
+            Some(each_attr) => {
+                quote! {
+                    #name: self.#name.clone().unwrap_or_default()
+                }
+            }
+            _ => {
+                quote! {
+                    #name: self.#name.clone()
+                        .ok_or_else(|| ::std::boxed::Box::<dyn ::std::error::Error>::from(
+                            ::std::format!("field `{}` is not set", ::std::stringify!(#name))
+                        ))?
+                }
+            }
         }
     }
 }
@@ -86,11 +159,12 @@ fn generate_empty_field(field: &Field) -> proc_macro2::TokenStream {
     quote! { #name: ::std::option::Option::None }
 }
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let struct_name = &ast.ident;
     let builder_name = syn::Ident::new(&format!("{}Builder", struct_name), struct_name.span());
+    // println!("{:#?}", ast);
 
     // Extract named fields from struct
     let fields = match &ast.data {

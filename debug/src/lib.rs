7096bea5a796c37507363d94d34b6bec8d24228d
Reza Kakhki <reza.kakhki@grover.com>
2025-08-04 16:58:56 +0200

    pass debug macro test 1

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _ = parse_macro_input!(input as DeriveInput);
+    quote! { /* ... */}.into()
 }

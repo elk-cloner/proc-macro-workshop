9bf5d5447452f3926943f2afa2a656b085e6af75
Reza Kakhki <reza.kakhki@grover.com>
2025-08-09 10:50:48 +0200

    pass debug macro test 3

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,38 @@
 use proc_macro::TokenStream;
 use quote::quote;
+
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+fn get_debug_attribute(field: &syn::Field) -> Option<proc_macro2::Literal> {
+    let attr = if field.attrs.len() != 0 {
+        &field.attrs[0]
+    } else {
+        return None;
+    };
+
+    let meta = match &attr.meta {
+        syn::Meta::NameValue(meta) => meta,
+        _ => {
+            return None;
+        }
+    };
+
+    let meta_value = if meta.path.segments.len() > 0 && meta.path.segments[0].ident == "debug" {
+        &meta.value
+    } else {
+        return None;
+    };
+
+    match &meta_value {
+        syn::Expr::Lit(syn::ExprLit {
+            lit: syn::Lit::Str(str),
+            ..
+        }) => Some(str.token()),
+        _ => None,
+    }
+}
+
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     // println!("{:#?}", ast);
@@ -25,7 +55,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let fmt_field_calls = fields.iter().map(|field| {
         let f_name = &field.ident;
-        quote! { .field(stringify!(#f_name), &self.#f_name)}
+
+        match get_debug_attribute(field) {
+            Some(format_string) => {
+                quote! { .field(stringify!(#f_name), &format_args!(#format_string, &self.#f_name))}
+            }
+            _ => {
+                quote! { .field(stringify!(#f_name), &self.#f_name)}
+            }
+        }
     });
 
     let expanded = quote! {
--- main.rs
+++ main.rs
@@ -16,6 +16,7 @@ use derive_debug::CustomDebug;
 #[derive(CustomDebug)]
 pub struct Field {
     name: &'static str,
+    #[debug = "0b{:08b}"]
     bitmask: u8,
 }
 

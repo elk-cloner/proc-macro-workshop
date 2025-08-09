7cc56fd7ace069205eb248aba6c5cbed7589bae5
Reza Kakhki <reza.kakhki@grover.com>
2025-08-09 09:17:50 +0200

    pass debug macro test 2

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -4,6 +4,39 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    quote! { /* ... */}.into()
+    let ast = parse_macro_input!(input as DeriveInput);
+    // println!("{:#?}", ast);
+
+    let struct_name = &ast.ident;
+    let fields = match &ast.data {
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+            ..
+        }) => named,
+        _ => {
+            return syn::Error::new_spanned(
+                &ast,
+                "CustomDebug only supports structs with named fields",
+            )
+            .into_compile_error()
+            .into()
+        }
+    };
+
+    let fmt_field_calls = fields.iter().map(|field| {
+        let f_name = &field.ident;
+        quote! { .field(stringify!(#f_name), &self.#f_name)}
+    });
+
+    let expanded = quote! {
+        impl ::std::fmt::Debug for #struct_name {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                f.debug_struct(stringify!(#struct_name))
+                    #(#fmt_field_calls)*
+                    .finish()
+            }
+        }
+    };
+
+    expanded.into()
 }
--- main.rs
+++ main.rs
@@ -1,12 +1,31 @@
-use derive_builder::Builder;
+// use derive_builder::Builder;
 
-#[derive(Builder)]
-pub struct Command {
-    executable: String,
-    #[builder(eac = "arg")]
-    args: Vec<String>,
-    env: Vec<String>,
-    current_dir: Option<String>,
+// #[derive(Builder)]
+// pub struct Command {
+//     executable: String,
+//     #[builder(eac = "arg")]
+//     args: Vec<String>,
+//     env: Vec<String>,
+//     current_dir: Option<String>,
+// }
+
+// fn main() {}
+
+use derive_debug::CustomDebug;
+
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u8,
 }
 
-fn main() {}
+fn main() {
+    // let f = Field {
+    //     name: "F",
+    //     bitmask: 0b00011100,
+    // };
+
+    // let debug = format!("{:?}", f);
+
+    // assert!(debug.starts_with(r#"Field { name: "F","#));
+}

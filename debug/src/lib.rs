47075405d432002ba85a90e01f50543e1df10ca7
Reza Kakhki <reza.kakhki@grover.com>
2025-08-10 17:23:51 +0200

    pass debug macro test 4

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -27,6 +27,16 @@ fn get_debug_attribute(field: &syn::Field) -> syn::Result<Option<proc_macro2::Li
     Ok(None)
 }
 
+fn generate_debug_bounds(param: &syn::GenericParam) -> Option<proc_macro2::TokenStream> {
+    match param {
+        syn::GenericParam::Type(type_param) => {
+            let ident = &type_param.ident;
+            return Some(quote! {#ident: ::std::fmt::Debug});
+        }
+        _ => None,
+    }
+}
+
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
@@ -65,8 +75,21 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let debug_bounds: Vec<_> = ast
+        .generics
+        .params
+        .iter()
+        .filter_map(generate_debug_bounds)
+        .collect();
+
     let expanded = quote! {
-        impl ::std::fmt::Debug for #struct_name {
+        impl #impl_generics ::std::fmt::Debug for #struct_name #ty_generics
+        where
+            #(#debug_bounds,)*
+            #where_clause
+        {
             fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                 f.debug_struct(stringify!(#struct_name))
                     #(#fmt_field_calls)*

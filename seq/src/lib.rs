19aaddb95d7a87a07e65995c0731850b0cefd123
Reza Kakhki <reza.kakhki@grover.com>
2025-08-22 20:36:20 +0200

    pass seq macro test 4

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,10 @@
+use std::collections::HashSet;
+
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::{quote, TokenStreamExt};
+use quote::TokenStreamExt;
 use syn::parse::{Parse, ParseStream};
-use syn::{parse_macro_input, DeriveInput, Token};
+use syn::{Token, parse_macro_input};
 
 #[derive(Debug)]
 struct SeqMacroInput {
@@ -48,23 +50,45 @@ impl Parse for SeqMacroInput {
     }
 }
 
-fn find_and_replace_N(input: TokenStream2, n_value: u8) -> TokenStream2 {
+fn find_and_replace_n(input: TokenStream2, n_value: u8) -> TokenStream2 {
+    let input: Vec<proc_macro2::TokenTree> = input.into_iter().collect();
     let mut token_stream = TokenStream2::new();
 
-    for token in input.clone() {
-        match &token {
+    let mut i = 0;
+    while i < input.len() {
+        match &input[i] {
             proc_macro2::TokenTree::Group(group) => {
-                let c = find_and_replace_N(group.stream(), n_value);
+                let c = find_and_replace_n(group.stream(), n_value);
                 let c =
                     proc_macro2::TokenTree::Group(proc_macro2::Group::new(group.delimiter(), c));
                 token_stream.append(c);
+                i += 1
             }
-            proc_macro2::TokenTree::Ident(ident) if ident.to_string() == "N" => {
-                let lit = syn::LitInt::new(&n_value.to_string(), ident.span());
-                token_stream.append(lit.token());
+            proc_macro2::TokenTree::Ident(ident) => {
+                // Look ahead for pattern: current_ident ~ N
+                if i + 2 < input.len()
+                    && let proc_macro2::TokenTree::Punct(punct) = &input[i + 1]
+                    && punct.to_string() == "~"
+                    && let proc_macro2::TokenTree::Ident(var) = &input[i + 2]
+                    && var.to_string() == "N"
+                {
+                    let combined = format!("{}{}", ident, n_value);
+                    let new_ident = proc_macro2::Ident::new(&combined, ident.span());
+                    token_stream.append(new_ident);
+                    i += 3; // Skip the ident, ~, and N
+                } else if ident.to_string() == "N" {
+                    // Standalone N replacement
+                    let lit = syn::LitInt::new(&n_value.to_string(), ident.span());
+                    token_stream.append(lit.token());
+                    i += 1;
+                } else {
+                    token_stream.append(input[i].clone());
+                    i += 1;
+                }
             }
             _ => {
-                token_stream.append(token);
+                token_stream.append(input[i].clone());
+                i += 1
             }
         };
     }
@@ -80,15 +104,15 @@ pub fn seq(input: TokenStream) -> TokenStream {
     let e: u8 = user_macro.range_end.base10_parse().unwrap();
     // println!("{:?}", s);
     // let res = find_and_replace_N(user_macro.body, 4);
-    // println!("here is the output: {:#?}", res);
+    // println!("here is the body: {:#?}", user_macro.body);
     let mut token_stream = TokenStream2::new();
 
     for i in s..e {
-        let processed_body = find_and_replace_N(user_macro.body.clone(), i);
-        println!("Iteration {}: {}", i, processed_body);
+        let processed_body = find_and_replace_n(user_macro.body.clone(), i);
+        // println!("Iteration {}: {}", i, processed_body);
         token_stream.extend(processed_body);
     }
-    println!("here is the output: {:?}", token_stream);
+    // println!("here is the output: {:?}", token_stream);
     // println!("here is the body: {:#?}", user_macro.body);
     token_stream.into()
 }
